600a67df3bd5cab9aa195bc6b15aac134c7eabf2
GuillermoEsquivel <38442205+GuillermoEsquivel@users.noreply.github.com>
2019-04-19 13:41:50 -0700

    Update main.py

--- main.py
+++ main.py
@@ -14,10 +14,10 @@ app = Flask(__name__)
 @app.route('/')
 def go_to_external_url():
     links = [
-    'https://whober.uberinternal.com/124039',
-    'https://whober.uberinternal.com/118023',
-    'https://whober.uberinternal.com/141329',
-    'https://whober.uberinternal.com/123826']
+    'https://workbench-dashboard.uberinternal.com/session/3b594d4b-8cde-5bbb-8243-7d7242942456/glweekly/',
+    'https://workbench.uberinternal.com/session/f006aa53-1673-5600-8914-ce532b5b607d/glweekly/',
+    'https://workbench.uberinternal.com/session/c6a72d33-6365-5c47-a767-212315b94db6/glweekly/'
+    ]
     link = random.sample(links, 1)[0]
     return redirect(link)
 
