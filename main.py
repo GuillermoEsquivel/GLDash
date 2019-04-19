75944cf39ab52eeb300f928a140e3f4daa0f09e8
GuillermoEsquivel <38442205+GuillermoEsquivel@users.noreply.github.com>
2019-04-19 14:10:39 -0700

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ app = Flask(__name__)
 def go_to_external_url():
     links = [
     'https://workbench-dashboard.uberinternal.com/session/3b594d4b-8cde-5bbb-8243-7d7242942456/glweekly/',
-    'https://workbench.uberinternal.com/session/f006aa53-1673-5600-8914-ce532b5b607d/glweekly/',
+    'https://workbench-dashboard.uberinternal.com/session/f006aa53-1673-5600-8914-ce532b5b607d/glweekly/',
     'https://workbench-dashboard.uberinternal.com/session/c6a72d33-6365-5c47-a767-212315b94db6/glweekly/'
     ]
     link = random.sample(links, 1)[0]
